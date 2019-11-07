ad3303c50c990d65685033c04ab805b7aa131d1d
paulafrodrigues <pfr.blackcat@gmail.com>
2019-11-06 22:21:27 -0300

    adicionando tentativa de desafio - não está funcionando!

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -8,8 +8,17 @@ const getAll = (request, response) => {
 
 const add = (request, response) => {
   let contato = request.body
+  let contatoNovo = contato
+  let contatoExistente = model.agenda
+  
+  //   for(let i = 0; i<contatoExistente.length; i++){
+  // if (contatoExistente[i] === contatoNovo){
+  return response.status(406).send("Cadastro duplicado!!")
+}
+    }
+  model.agenda.contatos.push(contato);
   contato.id = Math.random().toString(36).substr(-8)
- model.agenda.contatos.push(contato);
+  console.log(contato)
  response.status(200).send()
 
 }
