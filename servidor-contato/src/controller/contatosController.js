a2a8e9eedd102a209ef60bf1b5658e1f1c4cb2ac
paulafrodrigues <pfr.blackcat@gmail.com>
2019-11-07 22:18:52 -0300

    subindo possivel resolução do segundo desafio

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -12,9 +12,12 @@ const add = (request, response) => {
   contato.id = Math.random().toString(36).substr(-8)
     if(contatoExistente.find( existente => existente.nome === contato.nome)){
       response.status(400).send("Contato duplicado!")
+    }else if (contato.nome=== undefined  && contato.dataNascimento=== undefined && contato.celular=== undefined || contato.nome=== null && contato.dataNascimento=== null && contato.celular=== null ||
+      contato.nome.trim()=== "" && contato.dataNascimento.trim()=== "" &&  contato.celular.trim()=== ""|| contato.nome.length<0 && contato.dataNascimento.length<0 && contato.celular.length<0){     
+      response.status(400).send("Informações incorretas!")
+      console.log(contato)      
     }else{
-      model.agenda.contatos.push(contato)
-      
+      model.agenda.contatos.push(contato)      
       response.status(200).send(contato)
     }
 }
